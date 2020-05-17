01d09f195defcd7165c4911f81b4eea05055509e
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-05-17 03:53:43 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,5 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-  attr_accessor :email
+  attr_accessor :emails_raw, :parsed_arr
+  def initialize(list)
+    @email
 end
